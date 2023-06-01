e986169ef2e6bb3e4429c262d711ac618cd1486d
Sainath <saireddy1593@gmail.com>
2023-05-31 22:32:40 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("Sainath Reddy Moku");
+        System.out.println(s.getName());
+
+    }
+
+}
