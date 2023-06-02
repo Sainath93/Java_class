ede66e2e869d31515938d41d2d2e4183291648fa
Sainath <saireddy1593@gmail.com>
2023-06-02 15:22:30 -0500

    added

---
+++ src/AUDI.java
@@ -0,0 +1,9 @@
+public class AUDI extends Cars {
+    public AUDI() {
+    }
+
+    public int speed(int topspeed, int pickup) {
+        return topspeed + pickup;
+    }
+}
+
---
+++ src/Cars.java
@@ -0,0 +1,8 @@
+public class Cars {
+    public Cars() {
+    }
+
+    public int speed(int i, int speed) {
+        return speed;
+    }
+}
