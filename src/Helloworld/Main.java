0caeb7b307a5c8d98baca44682e9d252eeb48f9a
Sainath <saireddy1593@gmail.com>
2023-06-07 17:12:45 -0500

    Updated

--- src/Helloworld/Main.java
+++ src/Helloworld/Main.java
@@ -1,3 +1,5 @@
+package Helloworld;
+
 public class Main {
     public static void main(String[] args) {
         Student s = new Student();
--- src/Helloworld/Student.java
+++ src/Helloworld/Student.java
@@ -1,3 +1,5 @@
+package Helloworld;
+
 public class Student {
     String name;
     public String getName(){
